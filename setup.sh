a414de1b36b4191d0b212c54a81fb8e3647b0048
nvaikunt <nvaikunt@andrew.cmu.edu>
2022-10-31 15:51:05 -0400

    Fix: Change Setup Script to work with Colab

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+# conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
 conda install pytorch torchvision torchaudio -c pytorch-nightly
 pip install numpy
 pip install transformers
