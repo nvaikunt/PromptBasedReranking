1141582afbfe188a96f7accc4367e51ac275752e
nvaikunt <nvaikunt@andrew.cmu.edu>
2022-11-02 13:18:00 -0400

    Fix: Fix Setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
-conda install pytorch torchvision torchaudio -c pytorch-nightly
+conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
+# conda install pytorch torchvision torchaudio -c pytorch-nightly
 pip install numpy
 pip install transformers
 pip install tokenizers
