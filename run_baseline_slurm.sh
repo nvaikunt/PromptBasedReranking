478500453052c42eff836b7f0a904bd61e4ea6a3
nvaikunt <nvaikunt@andrew.cmu.edu>
2022-11-26 14:16:35 -0500

    Fix: Baseline Change

--- run_baseline_slurm.sh
+++ run_baseline_slurm.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #SBATCH --job-name=trn_qg_rnk_baseline
-#SBATCH --output /projects/tir5/users/nvaikunt/qgen_rnk_exp/log/output.txt
+#SBATCH --output /projects/tir5/users/nvaikunt/qgen_rnk_exp_true/log/output.txt
 #SBATCH --mem=32G
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:RTX_8000:3
+#SBATCH --gres=gpu:A6000:3
 #SBATCH --export=ALL
 source /home/nvaikunt/miniconda3/etc/profile.d/conda.sh
 conda activate baseline
 cd /home/nvaikunt/PromptBasedReRanking
-export QG=TRUE
-export RANKING=TRUE
+export QG=True
+export RANKING=True
 export PER_GPU_BATCH_SZ=45
-export OUTPUT_DIR=/projects/tir5/users/nvaikunt/qgen_rnk_exp/model
+export OUTPUT_DIR=/projects/tir5/users/nvaikunt/qgen_rnk_exp_true/model
 bash run_baseline.sh
