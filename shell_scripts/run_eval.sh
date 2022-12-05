78f8a9b5c2e6fae88a657c18498efdbc0e8aa51c
nvaikunt <nvaikunt@andrew.cmu.edu>
2022-12-04 19:28:59 -0500

    Fix: Minor Changes to Baseline Eval Script

--- shell_scripts/run_eval.sh
+++ shell_scripts/run_eval.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python evaluate.py --isQG "$QG" --isRanking "$RANKING" --max_eval_size "$EVAL_SZ" --outfile "$OUTFILE" \
 --evidence_dir "$EVIDENCE_DIR" --eval_data "$EVAL_DIR" -m "$MODEL" \
---batch_size "$EVAL_BATCH_SZ" --k "$K" --is_prompt "$IS_PROMPT" --run_name "$EVAL_RUN"\
---prompt_run_name "$PROMPT_RUN"
+--batch_size "$EVAL_BATCH_SZ" --k "$K" --is_prompt "$IS_PROMPT" --run_name "$EVAL_RUN" --prompt_run_name "$PROMPT_RUN"
