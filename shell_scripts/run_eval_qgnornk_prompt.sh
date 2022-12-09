faed5e0b34596941619c5e5d24d4bf11d3bfdad2
nvaikunt <nvaikunt@andrew.cmu.edu>
2022-12-08 21:28:50 -0500

    Fix: Changes to Prompt Eval Scripts

--- shell_scripts/run_eval_qgnornk_prompt.sh
+++ shell_scripts/run_eval_qgnornk_prompt.sh
@@ -15,11 +15,11 @@ export RANKING=False
 export PER_EVAL_BATCH_SZ=8
 export PROMPT_RUN="qgnornk_prompt"
 export IS_PROMPT=True
-export EVAL_RUN='qgen_nornk_eval_nq_test'
-export EVAL_DIR=/projects/tir5/users/nvaikunt/downloads/data/retriever-outputs/dpr/nq-test.json
-export OUTFILE=/projects/tir5/users/nvaikunt/qgen_nornk_exp/prompt_log/eval_metrics_baseline_nq_test.txt
-export MODEL=/projects/tir5/users/nvaikunt/qgen_nornk_exp/prompt_model/
-bash shell_scripts/run_eval.sh
+#export EVAL_RUN='qgen_nornk_eval_nq_test'
+#export EVAL_DIR=/projects/tir5/users/nvaikunt/downloads/data/retriever-outputs/dpr/nq-test.json
+#export OUTFILE=/projects/tir5/users/nvaikunt/qgen_nornk_exp/prompt_log/eval_metrics_baseline_nq_test.txt
+#export MODEL=/projects/tir5/users/nvaikunt/qgen_nornk_exp/prompt_model/
+#bash shell_scripts/run_eval.sh
 export EVAL_RUN='qgen_nornk_eval_squad1_test'
 export EVAL_DIR=/projects/tir5/users/nvaikunt/downloads/data/retriever-outputs/dpr/squad1-test.json
 export OUTFILE=/projects/tir5/users/nvaikunt/qgen_nornk_exp/prompt_log/eval_metrics_baseline_squad1_test.txt
