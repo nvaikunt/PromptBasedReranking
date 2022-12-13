aced541944bcb8fa1f38532fbe047d1f4bb02838
nvaikunt <nvaikunt@andrew.cmu.edu>
2022-12-13 11:21:43 -0500

    Fix: Debugging and Doing Temp Changes to Caching Behavior

--- shell_scripts/run_eval_rel_prompt.sh
+++ shell_scripts/run_eval_rel_prompt.sh
@@ -15,20 +15,21 @@ export IS_PROMPT=True
 export PROMPT_RUN="rel_prompt"
 export RANKING=False
 export PER_EVAL_BATCH_SZ=8
-export EVAL_RUN='rel_nornk_eval_nq_test'
-export EVAL_DIR=/projects/tir5/users/nvaikunt/downloads/data/retriever-outputs/dpr/nq-test.json
-export OUTFILE=/projects/tir5/users/nvaikunt/rel_nornk_exp/prompt_log/eval_metrics_baseline_nq_test.txt
+# export EVAL_RUN='rel_nornk_eval_nq_test'
+# export EVAL_DIR=/projects/tir5/users/nvaikunt/downloads/data/retriever-outputs/dpr/nq-test.json
+# export OUTFILE=/projects/tir5/users/nvaikunt/rel_nornk_exp/prompt_log/eval_metrics_baseline_nq_test.txt
 export MODEL=/projects/tir5/users/nvaikunt/rel_nornk_exp/prompt_model/
-bash shell_scripts/run_eval.sh
-export EVAL_RUN='rel_nornk_eval_squad1_test'
-export EVAL_DIR=/projects/tir5/users/nvaikunt/downloads/data/retriever-outputs/dpr/squad1-test.json
-export OUTFILE=/projects/tir5/users/nvaikunt/rel_nornk_exp/prompt_log/eval_metrics_baseline_squad1_test.txt
+# bash shell_scripts/run_eval.sh
+#export EVAL_RUN='rel_nornk_eval_squad1_test'
+#export EVAL_DIR=/projects/tir5/users/nvaikunt/downloads/data/retriever-outputs/dpr/squad1-test.json
+#export OUTFILE=/projects/tir5/users/nvaikunt/rel_nornk_exp/prompt_log/eval_metrics_baseline_squad1_test.txt
+#bash shell_scripts/run_eval.sh
+export EVAL_RUN='rel_nornk_eval_webq_test'
+export EVAL_DIR=/projects/tir5/users/nvaikunt/downloads/data/retriever-outputs/dpr/webq-test.json
+export OUTFILE=/projects/tir5/users/nvaikunt/rel_nornk_exp/prompt_log/eval_metrics_baseline_webq_test.txt
 bash shell_scripts/run_eval.sh
 export EVAL_RUN='rel_nornk_eval_trivia_test'
 export EVAL_DIR=/projects/tir5/users/nvaikunt/downloads/data/retriever-outputs/dpr/trivia-test.json
 export OUTFILE=/projects/tir5/users/nvaikunt/rel_nornk_exp/prompt_log/eval_metrics_baseline_trivia_test.txt
 bash shell_scripts/run_eval.sh
-export EVAL_RUN='rel_nornk_eval_webq_test'
-export EVAL_DIR=/projects/tir5/users/nvaikunt/downloads/data/retriever-outputs/dpr/webq-test.json
-export OUTFILE=/projects/tir5/users/nvaikunt/rel_nornk_exp/prompt_log/eval_metrics_baseline_webq_test.txt
-bash shell_scripts/run_eval.sh
+
